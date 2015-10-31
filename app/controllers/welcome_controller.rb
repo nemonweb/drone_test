bf242d13d2d8f6c8834e36f5e3e9fc0631144d86
Nikita Tikhomirov <nikitati@gmail.com>
2015-10-31 10:57:35 +0300

    mod

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
+  before_filter :add_item
   def index
+    @article = Article.first
+    @count = Article.count
+  end
+
+  private
+
+  def add_item
+    Article.create(title: 'test title', text: 'test text') if Article.all.count == 0
   end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe WelcomeController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
+      expect(assigns(:article)).to eq(Article.first)
+      expect(assigns(:count)).to eq(Article.count)
       expect(response).to have_http_status(:success)
     end
   end
